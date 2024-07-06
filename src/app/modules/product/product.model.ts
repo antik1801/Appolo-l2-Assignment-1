875952413c187cf2e2173f645994b4149b3eb4e3
antik1801 <antik.edu@gmail.com>
2024-07-06 14:54:41 +0600

    product.model.ts added

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,57 @@
+import { model, Schema } from "mongoose";
+import { TInventory, TProduct, TVariants } from "./product.interface";
+
+const VariantSchema = new Schema<TVariants>({
+    type: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: String,
+        required: true
+    }
+})
+
+const InventorySchema = new Schema<TInventory>({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    inStock: {
+        type: Boolean,
+        required: true
+    }
+})
+
+const ProductSchema = new Schema<TProduct>({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: [String],
+        required: true
+    },
+    variants:{
+        type: [VariantSchema],
+        required: true
+    },
+    inventory:{
+        type: InventorySchema,
+        required: true
+    }
+})
+
+const Products = model<TProduct>('Products', ProductSchema);
