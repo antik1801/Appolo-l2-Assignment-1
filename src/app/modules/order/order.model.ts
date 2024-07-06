4bd1b897f99b34eccdeb99c39dcb73d686685626
antik1801 <antik.edu@gmail.com>
2024-07-06 17:35:06 +0600

    order.model.ts --> order schema model created

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+import { TOrder } from "./order.interface";
+
+
+const OrderSchema = new Schema<TOrder>({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    productId: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+
+export const Order = model<TOrder>('Order', OrderSchema)
